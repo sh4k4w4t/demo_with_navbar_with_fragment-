9f21493a5c9bff38bf348ad6b766b44a0bca9642
Shakawat Hossain <mdshaheen227@gmail.com>
2021-07-04 16:49:20 -0400

    add default fragment

--- app/src/main/java/com/example/insurancedemo2/MainActivity.java
+++ app/src/main/java/com/example/insurancedemo2/MainActivity.java
@@ -6,11 +6,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Switch;
 
+import com.example.insurancedemo2.ui.home.HomeFragment;
+import com.example.insurancedemo2.ui.office.OfficeFragment;
 import com.google.android.material.navigation.NavigationView;
+
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -21,6 +29,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
 
         toolbar=(Toolbar) findViewById(R.id.main_activity_toolbar);
@@ -31,20 +44,47 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+
         navigationView=(NavigationView) findViewById(R.id.main_activity_navigation_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // Default Fragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.main_activity_linearlayout, new HomeFragment())
+                .commit();
+
+
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull @org.jetbrains.annotations.NotNull MenuItem item) {
 
-        if (item.getItemId()==R.id.nav_home){}
-        if (item.getItemId()==R.id.nav_bilicl_office){}
+
+        if (item.getItemId()==R.id.nav_home){
+            FragmentManager manager= getSupportFragmentManager();
+            FragmentTransaction transaction=manager.beginTransaction();
+
+            HomeFragment homeFragment= new HomeFragment();
+            transaction.replace(R.id.main_activity_linearlayout,homeFragment);
+            transaction.commit();
+        }
+        if (item.getItemId()==R.id.nav_bilicl_office){
+
+            FragmentManager manager= getSupportFragmentManager();
+            FragmentTransaction transaction=manager.beginTransaction();
+
+            OfficeFragment officeFragment= new OfficeFragment();
+            transaction.replace(R.id.main_activity_linearlayout,officeFragment);
+            transaction.commit();
+
+        }
         if (item.getItemId()==R.id.nav_bilicl_website){}
         if (item.getItemId()==R.id.nav_social_facebook){}
         if (item.getItemId()==R.id.nav_social_linkedin){}
         if (item.getItemId()==R.id.nav_social_mastodon){}
         if (item.getItemId()==R.id.nav_social_twitter){}
+
+
 
         drawerLayout.closeDrawer(GravityCompat.START);
         return false;
